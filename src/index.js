6892c717b2e8e414ad66884d7400308fd231388e
Giulio Riondino <riondino.giulio@gmail.com>
2025-01-06 21:40:30 -0800

    Add click interface - Folder Menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import './reset.css';
 import './styles.css';
 
-import Task from './components/Task'
-import Collection from './components/Collection'
+import Task from './components/Task';
+import Collection from './components/Collection';
+
+const sidebarItem = document.querySelectorAll('.sidebar .item')
+const inboxContainer = document.querySelector('.inbox');
+
+sidebarItem.forEach(item => {
+    item.addEventListener('click', () => {
+        sidebarItem.forEach(el => {
+            el.classList.remove('selected');
+        });
+        item.classList.add('selected');
+    });
+});
