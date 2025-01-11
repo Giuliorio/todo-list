0828f5c4fdf9a26de631e6e0d6a2e88b2344b72e
Giulio Riondino <riondino.giulio@gmail.com>
2025-01-11 09:59:43 -0800

    Reduce domHelper complexity by defaulting textContent to an empty string

--- src/helpers/domHelper.js
+++ src/helpers/domHelper.js
@@ -1,12 +1,10 @@
 export function createElement(tag, options = {}) {
-    const { classNames = [], textContent, attributes = {} } = options;
+    const { classNames = [], textContent = '', attributes = {} } = options;
     const element = document.createElement(tag);
 
     element.classList.add(...classNames);
 
-    if (textContent) {
-        element.textContent = textContent;
-    }
+    element.textContent = textContent;
 
     Object.keys(attributes).forEach(attribute => {
         element.setAttribute(attribute, attributes[attribute]);
