de73ec75da6c3b88fcc83f4487f715d5f1f00db9
Giulio Riondino <riondino.giulio@gmail.com>
2025-01-11 12:05:06 -0800

    Remove sidebar items from html, will render with JS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
+import Controller from './Controller';
 import './reset.css';
 import './styles.css';
 
-import Task from './components/Task';
-import Collection from './components/Collection';
-
-const sidebarItem = document.querySelectorAll('.sidebar .item')
-const inboxContainer = document.querySelector('.inbox');
-
-sidebarItem.forEach(item => {
-    item.addEventListener('click', () => {
-        sidebarItem.forEach(el => {
-            el.classList.remove('selected');
-        });
-        item.classList.add('selected');
-    });
-});
+Controller.init();
