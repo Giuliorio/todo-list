2c23dbc222474713e123eca63154379bd23a3fe5
Giulio Riondino <riondino.giulio@gmail.com>
2025-02-21 23:31:14 -0800

    ProjectManager.v2 Add swapProjects method

--- src/logic/ProjectManager.js
+++ src/logic/ProjectManager.js
@@ -17,6 +17,24 @@ class ProjectManager {
         this.#projects.filter(project => project != projectToRemove);
     }
 
+    swapProjects (id1, id2) {
+        let projects = this.#projects;
+        let index1 = -1, index2 = -1;
+
+        for (let i = 0; i < projects.length; i++) {
+            if (projects[i].id === id1) index1 = i;
+            if (projects[i].id === id2) index2 = i;
+            if(index !== -1 && index2 !== -1) break;
+        }
+
+        if (index1 === -1 && index2 === -1) {
+            console.error("One or both IDs not found!");
+            return;
+        }
+
+        [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+    }
+
     moveItem (itemId, fromProject, toProject) {
         fromProject.removeItem(itemId);
         toProject.addItem(itemId);
