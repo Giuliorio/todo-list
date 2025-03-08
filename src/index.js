d304484213c8c3157d443ea9949b96d7fc9b40a4
Giulio Riondino <riondino.giulio@gmail.com>
2025-03-08 08:36:33 -0800

    Change description to a textbox in order to make it functional

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './reset.css';
 import './styles.css';
+
+document.addEventListener("input", (event) => {
+    if (event.target.tagName.toLowerCase() === "textarea") {
+      event.target.style.height = "auto"; // Reset height
+      event.target.style.height = event.target.scrollHeight + "px"; // Set new height
+    }
+  });
