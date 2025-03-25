acdef95283d696144a701b19918f754cc7475d3a
Giulio Riondino <riondino.giulio@gmail.com>
2025-03-24 18:18:43 -0700

    Remove deprecated imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import createTask from './components/Task';
 import Controller from './controller/Controller';
-import { createElement } from './helpers/createElement';
 import './reset.css';
 import './styles.css';
 
